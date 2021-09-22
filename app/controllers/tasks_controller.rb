5d033217381891fef884c8b7b27c7a522870dd8a
hinata-bokko <chokichoki4869@gmail.com>
2021-09-23 02:58:23 +0900

    controller_create

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -2,4 +2,11 @@ class TasksController < ApplicationController
   def index
   end
   
+  def new
+
+  end
+
+  def create
+    render plain: "TASKを追加しました"
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
   get "tasks" => "tasks#index"
-
+  get "tasks/new" => "tasks#new"
+  post "tasks/create" => "tasks#create"
 end
+
+
